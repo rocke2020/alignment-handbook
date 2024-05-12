e055fcabb0d88cd56b504bd5504d54506e553b75
qichang.dong <qcdong@metanovas.com>
2024-05-12 22:31:38 +0800

    u

--- app/dpo/qlora.sh
+++ app/dpo/qlora.sh
@@ -7,9 +7,10 @@ fi
 export CUDA_VISIBLE_DEVICES=$gpu
 # --main_process_port=29555 \
 # --use_flash_attention_2=false \
+# deepspeed_zero3 multi_gpu
 nohup accelerate launch \
---config_file recipes/accelerate_configs/multi_gpu.yaml \
---num_processes=1 \
+--config_file recipes/accelerate_configs/deepspeed_zero2.yaml \
+--num_processes=2 \
 --main_process_port=29555 \
 scripts/run_dpo.py recipes/zephyr-7b-beta/dpo/config_qlora.yaml \
 > app/dpo/qlora.log 2>&1 &
